4144f13a217396bf2c8930495db55743afd2b0a0
Calle Englund <git@notcalle.xyz>
2022-08-05 09:55:49 +0200

    odin: Install vendor library collection
    Odin includes a large number of vendor library packages, but they were
    not included in the Formula. This installs the `vendor` collection, and
    adds a test that checks the `examples/all` package, verifying that all
    expected imports are available.

--- Formula/odin.rb
+++ Formula/odin.rb
@@ -29,7 +29,7 @@ class Odin < Formula
     inreplace "build_odin.sh", "dev-$(date +\"%Y-%m\")", "dev-#{version}" unless build.head?
 
     system "make", "release"
-    libexec.install "odin", "core", "shared"
+    libexec.install "odin", "core", "shared", "vendor"
     (bin/"odin").write <<~EOS
       #!/bin/bash
       export PATH="#{Formula["llvm"].opt_bin}:$PATH"
@@ -52,5 +52,9 @@ class Odin < Formula
     EOS
     system "#{bin}/odin", "build", "hellope.odin", "-file"
     assert_equal "Hellope!\n", shell_output("./hellope.bin")
+  end
+
+  test do
+    system "#{bin}/odin", "check", "#{pkgshare}/examples/all"
   end
 end
